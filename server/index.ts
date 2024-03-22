a0b0e4a1c5e29081dcbe439d92e96b4a05044bc4
okamoto-pmp <okamoto-k@dentsu-pmp.co.jp>
2024-03-22 16:31:44 +0900

    *chore: init

---
+++ server/index.ts
@@ -0,0 +1,21 @@
+import express from 'express'
+const app: express.Express = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "*")
+    res.header("Access-Control-Allow-Headers", "*");
+    next();
+})
+
+app.listen(3000, () => {
+    console.log("Start on port 3000.")
+})
+
+app.get('/', (req: express.Request, res: express.Response) => {
+    res.send(JSON.stringify({
+        message: "Hello, World!"
+    }))
+})
